e9e23f328b699f82afe96c03455b9cb31a04f7ae
wrnlb666 <wrnlb666@gmail.com>
2025-09-07 01:07:41 +1000

    installation script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+sudo -v
+
+# bashrc
+echo '[INFO] Installing config for bashrc'
+ln -sf "$(pwd)/bash/bashrc.sh" ~/.bashrc.local
+echo "# my bash config" >> ~/.bashrc
+echo "if [[ -r ~/.bashrc.local ]]; then" >> ~/.bashrc
+echo "    source ~/.bashrc.local" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+
+# hypr
+echo '[INFO] Installing config for hyprland'
+[[ -d ~/.config/hypr ]] || mkdir -p ~/.config/hypr
+for f in $(command ls hypr); do
+    ln -sf "$(pwd)/hypr/${f}" ~/.config/hypr/
+done
+echo '[WARN] Remeber to config `~/.config/hypr/monitors.conf` manually'
+
+# snapper
+echo '[INFO] Installing config for snapper'
+for c in "home" "root"; do
+    sudo ln -sf "$(pwd)/snapper/${c}" "/etc/snapper/configs/${c}"
+done
+echo '[WARN] Remeber to config `/etc/default/limine` manually'
