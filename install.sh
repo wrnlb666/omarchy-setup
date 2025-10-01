9c40cb2eeab6806c500e523baa826263c5c25ba5
wrnlb666 <wrnlb666@gmail.com>
2025-10-01 15:45:51 +1000

    set defualt terminal to alacritty, but launch ghostty with shortkey

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ echo '[INFO] Installing config for hyprland'
 [[ -d ~/.config/hypr ]] || mkdir -p ~/.config/hypr
 for f in $(command ls hypr); do
     [[ -f "~/.config/hypr/${f}" ]] && cp "~/.config/hypr/${f}" "~/.config/hypr/${f}.bak"
-    ln -f "$(pwd)/hypr/${f}" ~/.config/hypr/
+    ln -sf "$(pwd)/hypr/${f}" ~/.config/hypr/
 done
 echo '[WARN] Remeber to config `~/.config/hypr/monitors.conf` manually'
 
