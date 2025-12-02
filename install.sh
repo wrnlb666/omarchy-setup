c0077259c3a3eb86bf931ffc5c5706ea5ea791f9
wrnlb666 <wrnlb666@gmail.com>
2025-12-02 17:31:38 +1100

    add plugins

--- install.sh
+++ install.sh
@@ -14,6 +14,10 @@ fi
 # hypr
 echo '[INFO] Installing config for hyprland'
 [[ -d ~/.config/hypr ]] || mkdir -p ~/.config/hypr
+hyprpm update
+hyprpm add https://github.com/hyprwm/hyprland-plugins
+hyprpm enable hyprexpo
+hyprpm enable hyprscrolling
 for f in $(command ls hypr); do
     [[ -f "~/.config/hypr/${f}" ]] && cp "~/.config/hypr/${f}" "~/.config/hypr/${f}.bak"
     ln -sf "$(pwd)/hypr/${f}" ~/.config/hypr/
