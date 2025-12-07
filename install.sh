bffe592e18fff866dfb62c34318166f432d88de1
wrnlb666 <wrnlb666@gmail.com>
2025-12-07 13:28:46 +1100

    bashrc no longer live in this project

--- install.sh
+++ install.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 sudo -v
 
-# bashrc
-echo '[INFO] Installing config for bashrc'
-ln -sf "$(pwd)/bash/bashrc.sh" ~/.bashrc.local
-if ! grep -Fq '# my bash config' ~/.bashrc; then
-    echo "# my bash config" >> ~/.bashrc
-    echo "if [[ -r ~/.bashrc.local ]]; then" >> ~/.bashrc
-    echo "    source ~/.bashrc.local" >> ~/.bashrc
-    echo "fi" >> ~/.bashrc
-fi
-
 # hypr
 echo '[INFO] Installing config for hyprland'
 [[ -d ~/.config/hypr ]] || mkdir -p ~/.config/hypr
