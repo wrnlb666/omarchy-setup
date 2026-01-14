bb45237fa84022e1c0694f01180db4fc195016b5
wrnlb666 <wrnlb666@gmail.com>
2026-01-14 23:15:31 +1100

    install required packages in the script

--- install_hypr.sh
+++ install_hypr.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+sudo -v
 
 # hypr
 echo '[INFO] Installing config for hyprland'
 [[ -d ~/.config/hypr ]] || mkdir -p ~/.config/hypr
+sudo pacman -S --noconfirm --needed base-devel git cpio cmake meson ninja
 hyprpm update
 hyprpm add https://github.com/hyprwm/hyprland-plugins
 hyprpm enable hyprexpo
