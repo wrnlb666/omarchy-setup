86107290eae0d327bda5dd1ae247e3ea74d85562
wrnlb666 <wrnlb666@gmail.com>
2026-03-23 20:09:43 +1100

    add desc for chaotic aur

--- install_chaotic.sh
+++ install_chaotic.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo '[INFO] Installing chaotic AUR'
+
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 
