0a6776c19ee34e78cbf0c5c1dbac8f8e8c64863d
wrnlb666 <wrnlb666@gmail.com>
2026-08-20 01:50:32 +1000

    change dir name

--- install_env.sh
+++ install_env.sh
@@ -3,12 +3,12 @@
 # environment
 echo '[INFO] Installing environment variable'
 [[ -d ~/.config/environment.d ]] || mkdir -p ~/.config/environment.d
-for path in environment/*.conf; do
+for path in environment.d/*.conf; do
     envi="${path##*/}"
     [[ -f "${HOME}/.config/environment.d/${envi}" ]] &&
         cp \
             "${HOME}/.config/environment.d/${envi}" \
             "${HOME}/.config/environment.d/${envi}.bak"
-    ln -sf "$(pwd)/environment/${envi}" ~/.config/environment.d/
+    ln -sf "$(pwd)/environment.d/${envi}" ~/.config/environment.d/
 done
 echo "Logout and login again to take effect"
