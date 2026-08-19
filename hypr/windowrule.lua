e3a036ed40e05e8ee5f82046caf4ce6d62fb0ee0
wrnlb666 <wrnlb666@gmail.com>
2026-08-20 01:15:01 +1000

    update windowrule

--- hypr/windowrule.lua
+++ hypr/windowrule.lua
@@ -6,9 +6,10 @@ hl.workspace_rule({ workspace = "special:scratchpad", layout = "scrolling" })
 
 -- Open selected desktop applications as centered floating windows.
 local floating_apps = {
+	{ name = "QQ-spawn", match = { initial_class = "^(QQ)$" } },
 	{ name = "QQ", match = { initial_title = "^(QQ)$" }, size = { 1200, 800 } },
+	{ name = "wechat-wechat", match = { initial_class = "^(wechat)$" } },
 	{ name = "wechat-weixin", match = { initial_title = "^(Weixin)$" }, size = { 1200, 800 } },
-	{ name = "wechat-wechat", match = { initial_title = "^(WeChat)$" }, size = { 1200, 800 } },
 	{ name = "Bitwarden", match = { initial_class = "^(Bitwarden)$" }, size = { 1200, 800 } },
 	{ name = "localsend", match = { initial_title = "^(LocalSend)$" }, size = { 800, 600 } },
 	{ name = "AmneziaVPN", match = { initial_class = "^(AmneziaVPN)$" }, size = { 400, 800 } },
@@ -25,6 +26,4 @@ for _, app in ipairs(floating_apps) do
 	hl.window_rule({ name = app.name, match = app.match, float = true, center = true, size = app.size })
 end
 
-hl.window_rule({ match = { initial_class = "^(QQ)$" }, float = true, center = true })
-hl.window_rule({ match = { initial_class = "^(wechat)$" }, float = true, center = true })
 hl.window_rule({ match = { class = "^(Bitwarden)$" }, tag = "-floating-window" })
