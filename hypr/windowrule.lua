5de66a08f728253023724ee9fc094a394239af17
wrnlb666 <wrnlb666@gmail.com>
2026-08-21 19:38:36 +1000

    update windowrule

--- hypr/windowrule.lua
+++ hypr/windowrule.lua
@@ -9,10 +9,14 @@ local apps = {
 	-- { name = "QQ-spawn", match = { initial_class = "^(QQ)$" } },
 	-- { name = "QQ", match = { initial_title = "^(QQ)$" }, size = { 1200, 800 } },
 	-- { name = "wechat-weixin", match = { initial_title = "^(Weixin)$" }, size = { 1200, 800 } },
-	{ name = "wechat", match = { initial_title = "^(Weixin)$" }, float = false },
+	{
+		name = "wechat",
+		match = { initial_class = "^(wechat)$", initial_title = "^(Weixin|微信)$" },
+		float = false,
+	},
 	{
 		name = "wechat-float",
-		match = { initial_class = "^(wechat)$", initial_title = "negative:^(Weixin)$" },
+		match = { initial_class = "^(wechat)$", initial_title = "negative:^(Weixin|微信)$" },
 		float = true,
 		center = false,
 		border_size = 0,
@@ -40,12 +44,14 @@ local apps = {
 }
 
 for _, app in ipairs(apps) do
+	local should_float = app.float ~= false
 	hl.window_rule({
 		name = app.name,
 		match = app.match,
 		size = app.size,
-		float = app.float ~= false,
-		center = app.center ~= false,
+		float = should_float and true or nil,
+		tile = not should_float and true or nil,
+		center = should_float and (app.center ~= false) or nil,
 		border_size = app.border_size,
 		pin = app.pin,
 	})
