db2a2f07b8991b2699139d415e880522bf8f9543
wrnlb666 <wrnlb666@gmail.com>
2026-08-21 19:08:12 +1000

    update windowrule for wechat and codex-pet

--- hypr/windowrule.lua
+++ hypr/windowrule.lua
@@ -9,7 +9,23 @@ local apps = {
 	-- { name = "QQ-spawn", match = { initial_class = "^(QQ)$" } },
 	-- { name = "QQ", match = { initial_title = "^(QQ)$" }, size = { 1200, 800 } },
 	-- { name = "wechat-weixin", match = { initial_title = "^(Weixin)$" }, size = { 1200, 800 } },
-	{ name = "wechat-wechat", match = { initial_class = "^(wechat)$" }, float = false },
+	{ name = "wechat", match = { initial_title = "^(Weixin)$" }, float = false },
+	{
+		name = "wechat-float",
+		match = { initial_class = "^(wechat)$", initial_title = "negative:^(Weixin)$" },
+		float = true,
+		center = false,
+		border_size = 0,
+	},
+	{
+		name = "codex-pet",
+		match = {
+			initial_class = "^chatgpt$",
+			float = true,
+		},
+		border_size = 0,
+		pin = true,
+	},
 	{ name = "Bitwarden", match = { initial_class = "^(Bitwarden)$" }, size = { 1200, 800 } },
 	{ name = "localsend", match = { initial_title = "^(LocalSend)$" }, size = { 800, 600 } },
 	{ name = "AmneziaVPN", match = { initial_class = "^(AmneziaVPN)$" }, size = { 400, 800 } },
@@ -29,7 +45,9 @@ for _, app in ipairs(apps) do
 		match = app.match,
 		size = app.size,
 		float = app.float ~= false,
-		center = true ~= false,
+		center = app.center ~= false,
+		border_size = app.border_size,
+		pin = app.pin,
 	})
 end
 
