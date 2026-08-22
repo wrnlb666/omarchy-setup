abcecb623cc65ff6d52e1f3f7ae882e4d1ff4d47
wrnlb666 <wrnlb666@gmail.com>
2026-08-23 05:41:08 +1000

    add gesture for scroll move

--- hypr/input.lua
+++ hypr/input.lua
@@ -55,6 +55,7 @@ hl.config({
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Gestures/
 hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
 hl.gesture({ fingers = 3, direction = "swipe", mods = "SUPER", action = "resize" })
+hl.gesture({ fingers = 3, direction = "horizontal", mods = "ALT", action = "scroll_move" })
 hl.gesture({ fingers = 4, direction = "vertical", action = "special", workspace_name = "scratchpad" })
 
 -- mirador
